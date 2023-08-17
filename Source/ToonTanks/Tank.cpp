027e2359b6ebc68c5d492fdb94d3dda8cb5ecb4f
NikolayDimitrov69 <4mi0800004@g.fmi.uni-sofia.bg>
2023-08-17 11:27:15 +0300

    Update 0.3
    Added a Move function to Tank.h/.cpp
    Binded Move function to MoveForward input connected to W and S
    Added a MoveSpeed variable to Tank to make the speed configurable
    Added deltatime via GameplayStatics to account for fps changes when moving
    Tank can now move forwards and backwards

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -4,6 +4,7 @@
 #include "Tank.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 ATank::ATank()
 {
@@ -13,4 +14,22 @@ ATank::ATank()
     SpringArm->SetupAttachment(RootComponent);
 
     Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+}
+
+void ATank::Move(float Value)
+{
+    FVector DeltaLocation = FVector::ZeroVector;
+
+    double DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+
+    DeltaLocation.X = Value * DeltaTime * MoveSpeed;
+
+    AddActorLocalOffset(DeltaLocation);
 }
--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -13,14 +13,22 @@ UCLASS()
 class TOONTANKS_API ATank : public ABasePawn
 {
 	GENERATED_BODY()
+public:
+	ATank();
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* Camera;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* SpringArm;
-public:
 
-	ATank();
-	
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MoveSpeed = 300.f;
+
+	void Move(float Value);
 };
